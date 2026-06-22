ad40ed548bad93180b18b89213d2aa1442abbb5d
Nikita <nikita.leao.ua@gmail.com>
2026-06-23 00:43:07 +0300

    completed two tasks

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,50 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+populateForm();
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Submitted Data:', formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+}
+
+function populateForm() {
+  const savedState = localStorage.getItem(STORAGE_KEY);
+  if (savedState) {
+    try {
+      const parsedData = JSON.parse(savedState);
+
+      formData.email = parsedData.email || '';
+      formData.message = parsedData.message || '';
+
+      form.elements.email.value = formData.email;
+      form.elements.message.value = formData.message;
+    } catch (error) {
+      console.error('Error parsing JSON from localStorage:', error);
+    }
+  }
+}
